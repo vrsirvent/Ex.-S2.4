bc2710907df85516285668a2b6e57aacc137160d
vrsirvent <vrsirvent@gmail.com>
2025-11-19 22:33:41 +0100

    Update indexes.js

--- query/indexes.js
+++ query/indexes.js
@@ -1,1 +1,6 @@
-//Use this file to define indexes for your queries
+use("restaurants_db");
+
+db.restaurants.createIndex({ borough: 1 });
+db.restaurants.createIndex({ cuisine: 1 });
+db.restaurants.createIndex({ name: 1 });
+db.restaurants.createIndex({ "grades.score": 1 });
