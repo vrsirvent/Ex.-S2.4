726f4f857d054db7fc514a376160c80d1421ceff
vrsirvent <vrsirvent@gmail.com>
2025-11-19 23:25:01 +0100

    Update queries.js 02

--- query/queries.js
+++ query/queries.js
@@ -44,43 +44,43 @@ db.restaurants.find({ name: /^Wil/ }, { _id: 0, restaurant_id: 1, name: 1, borou
 db.restaurants.find({ name: /ces$/ }, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 // 16. Trobar restaurant_id, name, borough i cuisine on el nom conté 'Reg'.
-
+db.restaurants.find({ name: /Reg/ }, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 // 17. Trobar restaurants del Bronx que preparen cuina americana o xinesa.
-
+db.restaurants.find({ borough: "Bronx", $or: [ { cuisine: "American" }, { cuisine: "Chinese" } ] }, { _id: 0 });
 
 // 18. Trobar restaurant_id, name, borough i cuisine per a Staten Island, Queens, Bronx o Brooklyn.
-
+db.restaurants.find({ borough: { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }, { restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id: 0 });
 
 // 19. Trobar restaurant_id, name, borough i cuisine per a restaurants que NO són d'aquests barris.
-
+db.restaurants.find({ borough: { $nin: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 // 20. Trobar restaurant_id, name, borough i cuisine amb marcador no superior a 10.
-
+db.restaurants.find({ "grades.score": { $not: { $gt: 10 } } }, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 // 21. Trobar restaurants que preparen peix, no 'American' ni 'Chinees', o nom comença amb 'Wil'.
 
 
 // 22. Trobar restaurant_id, name, i grades per grau "A", score 11, i data "2014-08-11T00:00:00Z".
-
+db.restaurants.find({ grades: { $elemMatch: { grade: "A", score: 11, date: ISODate("2014-08-11T00:00:00Z") } } }, { restaurant_id: 1, name: 1, grades: 1, _id: 0 });
 
 // 23. Trobar restaurant_id, name i grades on el 2n element té grau "A", score 9 i data "2014-08-11T00:00:00Z".
-
+db.restaurants.find({ "grades.1.grade": "A", "grades.1.score": 9, "grades.1.date": ISODate("2014-08-11T00:00:00Z")}, { restaurant_id: 1, name: 1, grades: 1, _id: 0 });
 
 // 24. Trobar el restaurant_id, name, street, zipcode i coordenades dels restaurants a menys de 5 km de [-74, 40.7].
 
 
 // 25. Ordenar els noms dels restaurants en ordre ascendent, mostrant totes les columnes.
-
+db.restaurants.find({}, { _id: 0 }).sort({ name: 1 });
 
 // 26. Ordenar els noms dels restaurants en ordre descendent, mostrant totes les columnes.
-
+db.restaurants.find({}, { _id: 0 }).sort({ name: -1 });
 
 // 27. Ordenar cuisine ascendent i borough descendent.
-
+db.restaurants.find({}, { _id: 0 }).sort({ cuisine: 1, borough: -1 });
 
 // 28. Mostrar direccions que no contenen el carrer.
-
+db.restaurants.find({ "address.street": "" }, { _id: 0, address: 1 });
 
 // 29. Seleccionar documents on el valor de `coordinate` és de tipus Double. Mostrar el name, restaurant_id i coordinades.
 
@@ -92,3 +92,4 @@ db.restaurants.find({ name: /ces$/ }, { _id: 0, restaurant_id: 1, name: 1, borou
 
 
 // 32. Mostrar restaurant_id, name i grade i score de més de 80 però menys que 100.
+db.restaurants.find({ "grades.score": { $gt: 80, $lt: 100 } }, { _id: 0, restaurant_id: 1, name: 1, "grades.grade": 1, "grades.score": 1 });
